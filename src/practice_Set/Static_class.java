6e4b8597c00c96f4b262b9dd474523f29f168a4b
Ganesha <Ganesha@DESKTOP-KMBREE8>
2023-12-18 11:23:45 +0530

    [Pravin]Addding my First Code

---
+++ src/practice_Set/Static_class.java
@@ -0,0 +1,18 @@
+package practice_Set;
+
+public class Static_class {
+
+	static int a=7;
+	//@test
+	public static void show()
+	{
+		 a= a+5;
+		System.out.println(a);
+	}
+	public static void main(String[] args)
+	{
+		
+		show();
+	}
+
+}
